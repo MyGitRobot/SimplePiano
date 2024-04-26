c771c59a7e7f3ada7fe5e30f30420078eee81d9e
MyGitRobot <57513828+MyGitRobot@users.noreply.github.com>
2024-04-26 12:22:25 +0800

    Add py files and ui file

---
+++ simple_piano.py
@@ -0,0 +1,383 @@
+# -*- coding: utf-8 -*-
+
+"""
+Program: Simple Piano Assistant
+Author: MrCrawL
+Created Time: 2024-04-23
+Last Modified: 2024-04-26
+PS. 2024-04-24 by MrCrawL: Creat file and realize basic functions
+    2024-04-25 by MrCrawL: Add display information function and modify sound system
+    2024-04-26 by MrCrawL: Fix file not found bug and optimize code  # todo next step: add more soundLibs
+"""
+
+import sys, os
+from PyQt6.QtWidgets import QApplication, QWidget, QGroupBox, QPushButton, QMessageBox
+from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6.QtCore import QThread, pyqtSignal
+from xu_ui import Ui_Form
+from mr_ico import icon_hex  # todo: comment this line if Exception raised
+import pygame
+from pynput.keyboard import Listener
+import logging
+
+# 版本号
+VERSION = '1.0.0'
+
+# 获取文件路径
+filePath = None
+if getattr(sys, 'frozen', False):
+    filePath = os.path.join(os.path.dirname(sys.executable), os.path.basename(sys.executable))
+else:
+    filePath = os.path.abspath(__file__)
+fileDir = os.path.dirname(filePath)
+soundLibDir = os.path.join(fileDir, r'soundLib\Upright_Piano')
+
+# Ord 0-48, pick 32 from 49
+NOTE_LIST = ['C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+             'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+             'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+             'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6', 'C7']
+NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+ORD_MAP = {'C': 12, 'C#': 13, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11}
+ORDER = [0,2,4,5,7,9,11,12,14,16,17,19,21,23,24,26,28,29,31,1,3,6,8,10,13,15,18,20,22,25,27,30]
+SCALE_MAP = {
+    'Major (Ionian)': [0,2,4,5,7,9,11, 12,14,16,17,19,21,23, 24,26,28,29,31],
+    'Major Bebop': [0,2,4,5,7,8,9,11, 12,14,16,17,19,20,21,23, 24,26,28,29,31],
+    'Major Bulgarian': [0,2,5,6,8,9,11, 12,14,17,18,20,21,23, 24,26,29,30],
+    'Major Hexatonic': [0,2,4,5,7,9, 12,14,16,17,19,21, 24,26,28,29,31],
+    'Major Pentatonic': [0,2,4,7,9, 12,14,16,19,21, 24,26,28,31],
+    'Major Persian': [0,1,4,5,6,8,11, 12,13,16,17,18,20,23, 24,25,28,29,30],
+    'Major Polymode': [0,2,3,5,6,8,10,11, 12,14,15,17,18,20,22,23, 24,26,27,29,30],
+    'Minor Harmonic': [0,2,3,5,7,8,11, 12,14,15,17,19,20,23, 24,26,27,29,31],
+    'Minor Hungarian': [0,2,3,6,7,8,11, 12,14,15,18,19,20,23, 24,26,27,30,31],
+    'Minor Melodic': [0,2,3,5,7,9,11, 12,14,15,17,19,21,23, 24,26,27,29,31],
+    'Minor Natural (Aeolian)': [0,2,3,5,7,8,10, 12,14,15,17,19,20,22, 24,26,27,29,31],
+    'Minor Neapolitan': [0,1,3,5,7,8,11, 12,13,15,17,19,20,23, 24,25,27,29,31],
+    'Minor Pentatonic': [0,3,5,7,10, 12,15,17,19,22, 24,27,29,31],
+    'Minor Polymode': [0,1,2,4,5,7,8,10, 12,13,14,16,17,19,20,22, 24,25,26,28,29,31],
+    'Minor Romanian': [0,2,3,6,7,9,10, 12,14,15,18,19,21,22, 24,26,27,30,31],
+    'Other Arabic': [0,1,4,5,7,8,11, 12,13,16,17,19,20,23, 24,25,28,29,31],
+    'Other Bebop Dominant': [0,2,4,5,7,9,10,11, 12,14,16,17,19,21,22,23, 24,26,28,29,31],
+    'Other Blues': [0,3,5,6,7,10, 12,15,17,18,19,22, 24,27,29,30,31],
+    'Other Blues Nonatonic': [0,2,3,4,5,6,7,9,10, 12,14,15,16,17,18,19,21,22, 24,26,27,28,29,30,31],
+    'Other Diminished': [0,2,3,5,6,8,9,11, 12,14,15,17,18,20,21,23, 24,26,27,29,30],
+    'Other Dorian': [0,2,3,5,7,9,10, 12,14,15,17,19,21,22, 24,26,27,29,31],
+    'Other Eastern': [0,2,3,5,7,8,10,11, 12,14,15,17,19,20,22,23, 24,26,27,29,31],
+    'Other Egyptian': [0,2,5,7,10, 12,14,17,19,22, 24,26,29,31],
+    'Other Enigmatic': [0,1,4,6,8,10,11, 12,13,16,18,20,22,23, 24,25,28,30],
+    'Other Hirajoshi': [0,2,3,7,8, 12,14,15,19,20, 24,26,27,31],
+    'Other Iwato': [0,1,5,6,10, 12,13,17,18,22, 24,25,29,30],
+    'Other Japanese Insen': [0,1,5,7,10, 12,13,17,19,22, 24,25,29,31],
+    'Other Locrian': [0,1,3,5,6,8,10, 12,13,15,17,18,20,22, 24,25,27,29,30],
+    'Other Locrian Super': [0,1,3,4,6,8,10, 12,13,15,16,18,20,22, 24,25,27,28,30],
+    'Other Lydian': [0,2,4,6,7,9,11, 12,14,16,18,19,21,23, 24,26,28,30,31],
+    'Other Mixolydian': [0,2,4,5,7,9,10, 12,14,16,17,19,21,22, 24,26,28,29,31],
+    'Other Neapolitan': [0,1,3,5,7,9,11, 12,13,15,17,19,21,23, 24,25,27,29,31],
+    'Other Phrygian': [0,1,3,5,7,8,10, 12,13,15,17,19,20,22, 24,25,27,29,31],
+    'Other Phrygian Dominant': [0,1,4,5,7,8,10, 12,13,16,17,19,20,22, 24,25,28,29,31],
+    'Other Piongio': [0,2,5,7,9,10, 12,14,17,19,21,22, 24,26,29,31],
+    'Other Prometheus': [0,2,4,6,9,10, 12,14,16,18,21,22, 24,26,28,30],
+    'Other Whole Tone': [0,2,4,6,8,10, 12,14,16,18,20,22, 24,26,28,30],
+    'All': list(range(32)),
+}
+NUM_DICT = {'1':'I', '2':'II', '3':'III', '4':'IV', '5':'V', '6':'VI', '7':'VII', '8':'VIII', '9':'IX', '10':'X',
+            '11':'XI', '12':'XII'}
+
+
+class InfoThread(QThread):
+    """显示按下键盘的音高和音阶所在级数"""
+    finished = pyqtSignal()
+    info = pyqtSignal()
+
+    def run(self):
+        self.info.emit()
+        self.finished.emit()
+
+
+class KeyboardButton:
+    """包含各种属性的自定义键盘按钮"""
+    def __init__(self, push_button:QPushButton, note_name:str, state:bool=False):
+        self.button = push_button  # 键盘对应的 Qt 按钮
+        self.noteName = note_name  # 音名
+        self.isPressed = state  # 按键是否被按下
+        self.oggFile = os.path.join(soundLibDir, f'{self.noteName}.ogg')  # 音源文件路径
+        self.sound = pygame.mixer.Sound(self.oggFile)  # 能发声的 Sound 对象
+
+    def update_attrs(self, note_name:str):
+        """更新键盘按钮的属性"""
+        self.noteName = note_name
+        self.oggFile = os.path.join(soundLibDir, f'{self.noteName}.ogg')
+        self.sound = pygame.mixer.Sound(self.oggFile)
+    
+    def set_connections(self):
+        if self.button.receivers(self.button.pressed) > 0: self.button.pressed.disconnect()
+        if self.button.receivers(self.button.released) > 0: self.button.released.disconnect()
+        self.button.pressed.connect(lambda: Window.start_sound(self.sound))
+        self.button.released.connect(lambda: Window.stop_sound(self.sound))
+
+
+class Window(QWidget, Ui_Form):
+    """窗口 UI"""
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # 设置窗口图标 todo: comment 126-129 lines if Exception raised
+        self.pixmap = QPixmap()
+        self.pixmap.loadFromData(bytes.fromhex(icon_hex))
+        self.icon = QIcon(self.pixmap)
+        self.setWindowIcon(self.icon)
+        self.setWindowTitle(f'{self.windowTitle() + " " + VERSION}')
+
+        # 设置窗口固定大小
+        self.setFixedSize(800, 500)
+        # 调整黑键大小
+        self.resize_black_keys()
+        # 加载笔记内容
+        self.load_notepad()
+
+        # 调式选择
+        self.comboRootNote.setFocus()
+        self.comboRootNote.addItems(['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'])
+        self.comboScale.addItems(SCALE_MAP.keys())
+
+        self.rootNote = self.comboRootNote.currentText()
+        self.scale = self.comboScale.currentText()
+        self.scaleNoteOrds = SCALE_MAP[self.scale]
+        self.label_scale.setText(f'Scale : C  D  E  F  G  A  B')
+        # print(f'[Info] Root Note: {self.rootNote}, Scale: {self.scale}')
+        self.comboRootNote.currentTextChanged.connect(self.update_info)
+        self.comboScale.currentTextChanged.connect(self.update_info)
+        self.pressedNoteOrds = []
+
+        # 初始化键盘和音符
+        self.rootOrd = 12
+        try:
+            self.whiteKey_1 = KeyboardButton(self.buttonWhiteKey_1, NOTE_LIST[self.rootOrd])
+            self.whiteKey_2 = KeyboardButton(self.buttonWhiteKey_2, NOTE_LIST[self.rootOrd + 2])
+            self.whiteKey_3 = KeyboardButton(self.buttonWhiteKey_3, NOTE_LIST[self.rootOrd + 4])
+            self.whiteKey_4 = KeyboardButton(self.buttonWhiteKey_4, NOTE_LIST[self.rootOrd + 5])
+            self.whiteKey_5 = KeyboardButton(self.buttonWhiteKey_5, NOTE_LIST[self.rootOrd + 7])
+            self.whiteKey_6 = KeyboardButton(self.buttonWhiteKey_6, NOTE_LIST[self.rootOrd + 9])
+            self.whiteKey_7 = KeyboardButton(self.buttonWhiteKey_7, NOTE_LIST[self.rootOrd + 11])
+            self.whiteKey_8 = KeyboardButton(self.buttonWhiteKey_8, NOTE_LIST[self.rootOrd + 12])
+            self.whiteKey_9 = KeyboardButton(self.buttonWhiteKey_9, NOTE_LIST[self.rootOrd + 14])
+            self.whiteKey_10 = KeyboardButton(self.buttonWhiteKey_10, NOTE_LIST[self.rootOrd + 16])
+            self.whiteKey_11 = KeyboardButton(self.buttonWhiteKey_11, NOTE_LIST[self.rootOrd + 17])
+            self.whiteKey_12 = KeyboardButton(self.buttonWhiteKey_12, NOTE_LIST[self.rootOrd + 19])
+            self.whiteKey_13 = KeyboardButton(self.buttonWhiteKey_13, NOTE_LIST[self.rootOrd + 21])
+            self.whiteKey_14 = KeyboardButton(self.buttonWhiteKey_14, NOTE_LIST[self.rootOrd + 23])
+            self.whiteKey_15 = KeyboardButton(self.buttonWhiteKey_15, NOTE_LIST[self.rootOrd + 24])
+            self.whiteKey_16 = KeyboardButton(self.buttonWhiteKey_16, NOTE_LIST[self.rootOrd + 26])
+            self.whiteKey_17 = KeyboardButton(self.buttonWhiteKey_17, NOTE_LIST[self.rootOrd + 28])
+            self.whiteKey_18 = KeyboardButton(self.buttonWhiteKey_18, NOTE_LIST[self.rootOrd + 29])
+            self.whiteKey_19 = KeyboardButton(self.buttonWhiteKey_19, NOTE_LIST[self.rootOrd + 31])
+            self.blackKey_1 = KeyboardButton(self.buttonBlackKey_1, NOTE_LIST[self.rootOrd + 1])
+            self.blackKey_2 = KeyboardButton(self.buttonBlackKey_2, NOTE_LIST[self.rootOrd + 3])
+            self.blackKey_3 = KeyboardButton(self.buttonBlackKey_3, NOTE_LIST[self.rootOrd + 6])
+            self.blackKey_4 = KeyboardButton(self.buttonBlackKey_4, NOTE_LIST[self.rootOrd + 8])
+            self.blackKey_5 = KeyboardButton(self.buttonBlackKey_5, NOTE_LIST[self.rootOrd + 10])
+            self.blackKey_6 = KeyboardButton(self.buttonBlackKey_6, NOTE_LIST[self.rootOrd + 13])
+            self.blackKey_7 = KeyboardButton(self.buttonBlackKey_7, NOTE_LIST[self.rootOrd + 15])
+            self.blackKey_8 = KeyboardButton(self.buttonBlackKey_8, NOTE_LIST[self.rootOrd + 18])
+            self.blackKey_9 = KeyboardButton(self.buttonBlackKey_9, NOTE_LIST[self.rootOrd + 20])
+            self.blackKey_10 = KeyboardButton(self.buttonBlackKey_10, NOTE_LIST[self.rootOrd + 22])
+            self.blackKey_11 = KeyboardButton(self.buttonBlackKey_11, NOTE_LIST[self.rootOrd + 25])
+            self.blackKey_12 = KeyboardButton(self.buttonBlackKey_12, NOTE_LIST[self.rootOrd + 27])
+            self.blackKey_13 = KeyboardButton(self.buttonBlackKey_13, NOTE_LIST[self.rootOrd + 30])
+        except Exception as err:
+            self.msgbox('Notification', 'Sound files not found.\nPlease check your soundLib files.')
+            error_logging(err)
+            quit()
+
+        # 键盘按钮列表
+        self.buttonKeys = [self.whiteKey_1, self.blackKey_1, self.whiteKey_2, self.blackKey_2, self.whiteKey_3,
+                           self.whiteKey_4, self.blackKey_3, self.whiteKey_5, self.blackKey_4, self.whiteKey_6,
+                           self.blackKey_5, self.whiteKey_7,
+                           self.whiteKey_8, self.blackKey_6, self.whiteKey_9, self.blackKey_7, self.whiteKey_10,
+                           self.whiteKey_11,self.blackKey_8, self.whiteKey_12,self.blackKey_9, self.whiteKey_13,
+                           self.blackKey_10,self.whiteKey_14,
+                           self.whiteKey_15,self.blackKey_11,self.whiteKey_16,self.blackKey_12,self.whiteKey_17,
+                           self.whiteKey_18,self.blackKey_13,self.whiteKey_19]
+
+        self.keyMap = {'z': self.whiteKey_1, 'x': self.whiteKey_2, 'c': self.whiteKey_3, 'v': self.whiteKey_4,
+                       'b': self.whiteKey_5, 'n': self.whiteKey_6, 'm': self.whiteKey_7, ',': self.whiteKey_8,
+                       '.': self.whiteKey_9, '/': self.whiteKey_10,'q': self.whiteKey_8, 'w': self.whiteKey_9,
+                       'e': self.whiteKey_10,'r': self.whiteKey_11,'t': self.whiteKey_12,'y': self.whiteKey_13,
+                       'u': self.whiteKey_14,'i': self.whiteKey_15,'o': self.whiteKey_16,'p': self.whiteKey_17,
+                       '[': self.whiteKey_18,']': self.whiteKey_19,
+                       's': self.blackKey_1, 'd': self.blackKey_2, 'g': self.blackKey_3, 'h': self.blackKey_4,
+                       'j': self.blackKey_5, 'l': self.blackKey_6, ';': self.blackKey_7, '2': self.blackKey_6,
+                       '3': self.blackKey_7, '5': self.blackKey_8, '6': self.blackKey_9, '7': self.blackKey_10,
+                       '9': self.blackKey_11,'0': self.blackKey_12,'=': self.blackKey_13}
+
+        # 显示音符
+        self.keyOriginText = []
+        self.keyOnScaleText = []
+        # 信息线程
+        self.infoThread = InfoThread()
+        self.infoThread.info.connect(self.setInfoText)
+        self.infoThread.finished.connect(self.display_info)
+
+        # 创建键盘监听
+        self.is_listening = False
+        self.listener = Listener(on_press=self.on_press, on_release=self.on_release)
+        self.listener.start()
+
+        # 设置键盘焦点事件
+        self.boxKeyboard.focusInEvent = self.keyboardFocusInEvent
+        self.boxKeyboard.focusOutEvent = self.keyboardFocusOutEvent
+
+        # 绑定键盘信号槽
+        self.buttonSaveNote.clicked.connect(self.save_notepad)  # 保存按钮
+        self.build_connections()
+        self.buttonSwitch.clicked.connect(self.switch_keys)
+
+    def setInfoText(self):
+        self.keyOriginText = [button.noteName for button in self.buttonKeys if button.isPressed]
+        keyNoteOrds = [NOTE_LIST.index(noteName)-self.rootOrd+12 for noteName in self.keyOriginText]  # [12, 16, 19]
+        for i in range(len(keyNoteOrds)):
+            while keyNoteOrds[i] > 11:
+                keyNoteOrds[i] -= 12
+        self.keyOnScaleText = [NUM_DICT[str(self.scaleNoteOrds.index(ele)+1)] if ele in self.scaleNoteOrds else '#'
+                               for ele in keyNoteOrds]
+
+    def display_info(self):
+        self.lineKeyOrigin.setText('  '.join(self.keyOriginText))
+        self.lineKeyOnScale.setText('  '.join(self.keyOnScaleText))
+
+    def update_info(self):
+        self.rootNote = self.comboRootNote.currentText()
+        self.rootOrd = ORD_MAP[self.rootNote]
+        self.scale = self.comboScale.currentText()
+        self.scaleNoteOrds = SCALE_MAP[self.scale]
+        scaleOrds = [num for num in self.scaleNoteOrds if num < 12]  # 0, 2, 3, 5
+        scaleOrds = [(num+self.rootOrd)%12 for num in scaleOrds]
+        scaleNotes = [NOTE_NAMES[num] for num in scaleOrds]
+        self.label_scale.setText(f'Scale : {"  ".join(scaleNotes)}')
+        self.label_info.setText(f'State: display information as scale {self.rootNote} {self.scale}')
+        # print(f'[Info] Root Note: {self.rootNote}, Scale: {self.scale}, Scale Ords: {self.scaleNoteOrds}')
+
+    def switch_keys(self):
+        """更换键盘绑定音高"""
+        # print(f'[Info] Root Ord: {self.rootOrd}')
+        for i in range(len(self.buttonKeys)):
+            self.buttonKeys[i].update_attrs(NOTE_LIST[self.rootOrd + i])
+        self.label_info.setText('State: keys switched to current scale')
+
+    def on_press(self, key):
+        """按下键盘发出声音"""
+        if hasattr(key, 'char'):
+            if self.is_listening and key.char in self.keyMap.keys():
+                if not self.keyMap[key.char].isPressed:
+                    self.infoThread.start()
+                    self.keyMap[key.char].isPressed = True
+                    self.keyMap[key.char].button.setDown(True)
+                    self.start_sound(self.keyMap[key.char].sound)
+
+    def on_release(self, key):
+        """释放按键停止声音"""
+        if hasattr(key, 'char'):
+            if self.is_listening and key.char in self.keyMap.keys():
+                self.infoThread.start()
+                self.keyMap[key.char].isPressed = False
+                self.keyMap[key.char].button.setDown(False)
+                self.stop_sound(self.keyMap[key.char].sound)
+
+    @staticmethod
+    def start_sound(note: pygame.mixer.Sound):
+        note.set_volume(100)
+        note.play()
+
+    @staticmethod
+    def stop_sound(note: pygame.mixer.Sound):
+        for i in range(100, 0, -10):  # 减弱
+            pygame.time.delay(10)
+            note.set_volume(i/100)
+        note.stop()
+
+    def keyboardFocusInEvent(self, event):
+        self.is_listening = True
+        QGroupBox.focusInEvent(self.boxKeyboard, event)
+
+    def keyboardFocusOutEvent(self, event):
+        self.is_listening = False
+        for buttonKey in self.buttonKeys:
+            buttonKey.button.setDown(False)
+            buttonKey.isPressed = False
+        QGroupBox.focusOutEvent(self.boxKeyboard, event)
+
+    def build_connections(self):
+        for buttonKey in self.buttonKeys:
+            buttonKey.set_connections()
+
+    def closeEvent(self, event):
+        """退出程序时保存笔记内容"""
+        self.save_notepad()
+        self.is_listening = False
+        event.accept()
+
+    def load_notepad(self):
+        """加载 notepad.txt"""
+        if not os.path.exists(os.path.join(fileDir, 'notepad.txt')):
+            with open(os.path.join(fileDir, 'notepad.txt'), 'w', encoding='utf-8') as f:
+                f.write('')
+        with open('notepad.txt', 'r', encoding='utf-8') as f:
+            self.textNotePad.setText(f.read())
+        self.label_info.setText('State: NotePad loaded successfully')
+        # print('[Info] NotePad loaded')
+
+    def save_notepad(self):
+        """保存 notepad.txt"""
+        with open('notepad.txt', 'w', encoding='utf-8') as f:
+            f.write(self.textNotePad.toPlainText())
+        self.label_info.setText('State: NotePad saved')
+        # print('[Info] NotePad saved')
+
+    def resize_black_keys(self):
+        """调整所有黑键宽度"""
+        buttonBlackKeys = [self.buttonBlackKey_1, self.buttonBlackKey_2, self.buttonBlackKey_3, self.buttonBlackKey_4,
+                           self.buttonBlackKey_5, self.buttonBlackKey_6, self.buttonBlackKey_7, self.buttonBlackKey_8,
+                           self.buttonBlackKey_9, self.buttonBlackKey_10, self.buttonBlackKey_11,
+                           self.buttonBlackKey_12, self.buttonBlackKey_13]
+        for button in buttonBlackKeys:
+            button.setGeometry(button.x(), button.y(), button.width() - 1, button.height())
+
+    def msgbox(self, title:str, text:str):
+        """对话框"""
+        msgBox = QMessageBox()
+        msgBox.setWindowIcon(self.icon)  # todo: comment this line if Exception raised
+        msgBox.setWindowTitle(title)
+        msgBox.setText(text)
+        msgBox.setIcon(QMessageBox.Icon.Information)
+        msgBox.addButton(QMessageBox.StandardButton.Ok)
+        msgBox.exec()
+
+
+# 记录错误日志
+def error_logging(error:Exception):
+    filename = os.path.join(fileDir, 'error.log')
+    logging.basicConfig(filename=filename, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.ERROR,
+                        encoding='utf-8')
+    logging.error(f"Error o rccurred: {error}", exc_info=True)
+
+
+def main():
+
+    # 初始化 pygame，使用 pygame 发出声音
+    pygame.init()
+    pygame.mixer.init()
+
+    # 初始化 Qt 窗口
+    app = QApplication(sys.argv)
+    ui = Window()
+    ui.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'[Error] {e}')
+        error_logging(e)
